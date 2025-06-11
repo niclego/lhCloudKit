0d56bd7c9dbf50a674246043f33ad7caaf408691
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2025-06-11 18:47:57 -0400

    Update follower fetching to use follower records

--- Sources/lhCloudKit/Queries/UserFollowerQuery.swift
+++ Sources/lhCloudKit/Queries/UserFollowerQuery.swift
@@ -9,11 +9,14 @@ import Foundation
 public extension Query {
     enum UserFollowerQuery {
         case isFollowing(String, String)
+        case getFollowersForFollowee(String)
 
         var query: CloudKitQuery {
             switch self {
             case .isFollowing(let follower, let followee):
                 return isFollowing(follower: follower, followee: followee)
+            case .getFollowersForFollowee(let followee):
+                return getFollowers(for: followee)
             }
         }
 
@@ -22,6 +25,15 @@ public extension Query {
                 recordType: LhUserFollower.LhUserFollowerRecordKeys.type.rawValue,
                 sortDescriptorKey: LhUserFollower.LhUserFollowerRecordKeys.created.rawValue,
                 predicate: NSPredicate(format: "follower == %@ AND followee == %@", follower, followee),
+                database: .pubDb
+            )
+        }
+
+        private func getFollowers(for followee: String) -> CloudKitQuery {
+            return .init(
+                recordType: LhUserFollower.LhUserFollowerRecordKeys.type.rawValue,
+                sortDescriptorKey: LhUserFollower.LhUserFollowerRecordKeys.created.rawValue,
+                predicate: NSPredicate(format: "followee == %@", followee),
                 database: .pubDb
             )
         }
