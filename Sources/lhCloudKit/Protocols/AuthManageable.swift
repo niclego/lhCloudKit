79aaadfa7c32e1caca7210d4ae9ca200c9c3e8cb
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2025-07-06 20:17:30 -0400

    Add AuthManager protocol and stubs

---
+++ Sources/lhCloudKit/Protocols/AuthManageable.swift
@@ -0,0 +1,13 @@
+//
+//  AuthManageable.swift
+//
+//
+//  Created by Nicolas Le Gorrec on 8/8/24.
+//
+
+import Foundation
+
+public protocol AuthManageable: Sendable {
+    func signInWithAppleIdToken(_ token: String) async throws
+    func signOut() async throws
+}
