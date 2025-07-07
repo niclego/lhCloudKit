452cab8b242913516062a01d4efa52860d9c6281
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2025-07-06 21:41:02 -0400

    Add LhAuthChangeEvent enum and authStateChanges

--- Sources/lhCloudKit/Manager/Auth/AuthManager.swift
+++ Sources/lhCloudKit/Manager/Auth/AuthManager.swift
@@ -10,6 +10,13 @@ import Foundation
 public struct AuthManager: AuthManageable {
     public init() { }
 
+    public var authStateChanges: AsyncStream<(LhAuthChangeEvent, Bool)> {
+        AsyncStream { continuation in
+            // TODO: implement auth state change events
+            continuation.finish()
+        }
+    }
+
     public func signInWithAppleIdToken(_ token: String) async throws {
         // TODO: implement sign in with Apple ID token
     }
--- Sources/lhCloudKit/Manager/Auth/AuthManagerMock.swift
+++ Sources/lhCloudKit/Manager/Auth/AuthManagerMock.swift
@@ -10,6 +10,12 @@ import Foundation
 public struct AuthManagerMock: AuthManageable {
     public init() {}
 
+    public var authStateChanges: AsyncStream<(LhAuthChangeEvent, Bool)> {
+        AsyncStream { continuation in
+            continuation.finish()
+        }
+    }
+
     public func signInWithAppleIdToken(_ token: String) async throws { }
 
     public func signOut() async throws { }
